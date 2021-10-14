aa1115c36fcbee41706914ad158a311699d48ef5
lmihaig <licu.mihai21@gmail.com>
2021-10-15 02:47:34 +0300

    finished query feature worked on pie graph

--- analyser.py
+++ analyser.py
@@ -1,9 +1,10 @@
 import re
 from datetime import date
-from base64 import b64decode
+from base64 import b64decode, b64encode
 import dash
+from dash.html.Div import Div
 import diskcache
-from dash import dcc, html
+from dash import dcc, html, dash_table
 from dash.long_callback import DiskcacheLongCallbackManager
 from dash.dependencies import Output, Input, State
 import dash_bootstrap_components as dbc
@@ -134,99 +135,103 @@ app.layout = dbc.Container(fluid=True, children=[
                        })
         ])
     ], className="mt-2 mb-2"),
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("TOTAL MESSAGES", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="total-messages")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("TOTAL EMOJIS", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="total-emojis")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("TOTAL LINKS", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="total-links")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("TOTAL MEDIA", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="total-media")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3)
-    ], className="mb-2"),
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("TOTAL CHATTERS", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="total-chatters")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("MOST USED EMOJI", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="most-used-emoji")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("PERSON WITH MOST MESSAGES", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="most-messages-person")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("DAY WITH MOST MESSAGES", style={
-                               "textAlign": "center"}),
-                dbc.CardBody([
-                    html.H2(id="most-messages-day")
-                ], style={"textAlign": "center"})
-            ])
-        ], width=3)
-    ], className="mb-2"),
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id="line-chart", figure={})
+
+    dcc.Store(id="dataframe"),
+
+    html.Div([
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("TOTAL MESSAGES", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="total-messages")
+                    ], style={"textAlign": "center"})
                 ])
-            ])
-        ], width=6),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id="bar-chart", figure={})
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("TOTAL EMOJIS", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="total-emojis")
+                    ], style={"textAlign": "center"})
                 ])
-            ])
-        ], width=6),
-    ], className="mb-2"),
-    dbc.Row([
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("TOTAL LINKS", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="total-links")
+                    ], style={"textAlign": "center"})
+                ])
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("TOTAL MEDIA", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="total-media")
+                    ], style={"textAlign": "center"})
+                ])
+            ], width=3)
+        ], className="mb-2"),
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("TOTAL CHATTERS", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="total-chatters")
+                    ], style={"textAlign": "center"})
+                ])
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("MOST USED EMOJI", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="most-used-emoji")
+                    ], style={"textAlign": "center"})
+                ])
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("PERSON WITH MOST MESSAGES", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="most-messages-person")
+                    ], style={"textAlign": "center"})
+                ])
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("DAY WITH MOST MESSAGES", style={
+                        "textAlign": "center"}),
+                    dbc.CardBody([
+                        html.H2(id="most-messages-day")
+                    ], style={"textAlign": "center"})
+                ])
+            ], width=3)
+        ], className="mb-2"),
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        dcc.Graph(id="line-chart", figure={})
+                    ])
+                ])
+            ], width=6),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        dcc.Graph(id="bar-chart", figure={})
+                    ])
+                ])
+            ], width=6),
+        ], className="mb-2"),
+        dbc.Row([
             dbc.Col([
                 dbc.Card([
                     dbc.CardBody([
@@ -241,11 +246,16 @@ app.layout = dbc.Container(fluid=True, children=[
                     ])
                 ])
             ], width=6),
-            ], className="mb-2")
+        ], className="mb-2")
+    ], id="cards-and-graphs", style={"display": "none"}),
+
+    html.Div([],
+             id="query-results", style={"display": "none"})
 ])
 
 
 @ app.callback(
+    Output("cards-and-graphs", "style"),
     Output("total-messages", "children"),
     Output("total-emojis", "children"),
     Output("total-links", "children"),
@@ -258,13 +268,12 @@ app.layout = dbc.Container(fluid=True, children=[
     Output("pie-chart", "figure"),
     Output("bar-chart", "figure"),
     Output("word-cloud", "figure"),
+    Output("dataframe", "data"),
     Input("submit-val", "n_clicks"),
-    State("queryword", "value"),
     State("upload-chat", "contents"),
     State("upload-chat", "filename"),
-    State("upload-chat", "last_modified"),
 )
-def update_cards(click, queryword, contents, filename, last_modified):
+def update_cards(click, contents, filename):
     content_type, content_string = contents.split(",")
     decoded_text = b64decode(content_string).decode("utf-8")
     try:
@@ -281,8 +290,26 @@ def update_cards(click, queryword, contents, filename, last_modified):
     # total non-link non-media non-system messages
     total_messages = len(df)
 
+    # total messages with links
+    total_links = len(df_links)
+
+    # total messages with media
+    total_media = len(df_media)
+
     # all the people that have ever sent a message in this chat
     total_chatters = len(df["Name"].unique())
+
+    # most used emoji in texsts
+
+    # person that sent the most messages
+    messages_per_chatter = df['Name'].value_counts()
+    most_messages_person = messages_per_chatter.index.tolist()[0]
+    messages_per_chatter = messages_per_chatter.to_frame()
+
+    # day that had most activity
+    messages_per_day = df['Date'].value_counts()
+    most_messages_day = messages_per_day.index.tolist()[0]
+    messages_per_day = messages_per_day.to_frame()
 
     # figure = px.line(df_c, x=
     # slow as fuck 6-7s
@@ -290,6 +317,19 @@ def update_cards(click, queryword, contents, filename, last_modified):
     # come back to this later, don't forget to return the right thing
     # emojilist = df_c["Content"].str.extract(emoji.get_emoji_regexp()).dropna()
 
+    # line chart with x
+    print(messages_per_chatter)
+    # pie chart with messages per sender
+    fig_piechart = px.pie(messages_per_chatter, values="",
+                          names="Names", title="Messages per Sender")
+    fig_piechart.update_layout(
+        plot_bgcolor='rgba(0, 0, 0, 0)', paper_bgcolor='rgba(0, 0, 0, 0)', title_x=0.5, margin=dict(l=20, r=20, t=30, b=20))
+    fig_piechart.update_xaxes(visible=False)
+    fig_piechart.update_yaxes(visible=False)
+
+    # barchart with x
+
+    # wordcloud with messages
     wordcloud = WordCloud(background_color="rgb(48,48,48)", min_word_length=4, width=800, height=400).generate(
         " ".join(df["Content"].astype(str)))
     fig_wordcloud = px.imshow(
@@ -299,8 +339,39 @@ def update_cards(click, queryword, contents, filename, last_modified):
     fig_wordcloud.update_xaxes(visible=False)
     fig_wordcloud.update_yaxes(visible=False)
 
-    return total_messages, 1, 1, 1, total_chatters, 1, 1, 1, 1, 1, 1, fig_wordcloud
-    # return total_messages, total_emojis, total_links, total_media, total_chatters, most_used_emoji, most_messages_person, most_messages_day, fig_linechart, fig_piechart, fig_barchart, fig_wordcloud
+    visibility = {"display": "block"}
+    df = df.to_json()
+    return visibility, total_messages, 1, total_links, total_media, total_chatters, 1, most_messages_person, most_messages_day, 1, fig_piechart, 1, fig_wordcloud, df
+    # return visibility, total_messages, total_emojis, total_links, total_media, total_chatters, most_used_emoji, most_messages_person, most_messages_day, fig_linechart, fig_piechart, fig_barchart, fig_wordcloud, dataframe
+
+
+@app.callback(Output("query-results", "style"), Output("query-results", "children"),  Input("submit-val", "n_clicks"), State("queryword", "value"), State("dataframe", "data"))
+def update_query(click, queryword, df):
+    visibility = {"display": "none"}
+    if queryword is not None:
+        df = pd.read_json(df)
+        df_query = pd.DataFrame(columns=df.columns)
+        rows = df[df["Content"].astype(str).str.contains(
+            f"{queryword}|{queryword.upper()}")]
+        df_query = df_query.append(rows, ignore_index=True)
+        visibility = {"display": "block"}
+        table = [html.Hr(), html.H2("Query Results"),
+                 dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in df_query.columns],
+            page_size=20,
+            data=df_query.to_dict('records'),
+            style_as_list_view=True,
+            style_header={'backgroundColor': 'rgb(30, 30, 30)'},
+            style_cell={
+                'backgroundColor': 'rgb(50, 50, 50)',
+                'color': 'white',
+                'textAlign': 'left'
+            }
+        )]
+
+        return visibility, table
+    return visibility
 
 
 if __name__ == "__main__":
